1d85c70b05a862af07a7574a634c7bc6d73fde70
dataking <dataking@gmail.com>
2017-05-31 22:41:18 -0700

    WIP

--- pajak.pl
+++ pajak.pl
@@ -3,9 +3,10 @@
 use strict;
 use warnings;
 
+use Term::ANSIColor;
 use WWW::Mechanize;
-
-use Getopt::Long qw( no_ignore_case bundling );
+use Data::Dumper;
+use Getopt::Long qw( :config no_ignore_case bundling );
 
 my ($help, $verbose, $linksfile, $mailsfile, $starturl);
 GetOptions(
@@ -24,4 +25,13 @@ if ((defined($starturl)) and ($starturl ne '')) {
 	push @new_urls, $starturl;
 } else {
 	die "You must specify a starting URL!";
+}
+
+while (scalar(@new_urls) > 0) {
+	my $thisurl = shift @new_urls;
+	$mech->get($thisurl);
+	if ($mech->success) {
+		my @links = $mech->find_all_links();
+		#print Dumper(\@links);
+	}
 }
